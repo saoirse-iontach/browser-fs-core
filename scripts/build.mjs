ca070ba816d05b25986501ebfd2639c1c4f5e72d
saoirse-iontach <114183267+saoirse-iontach@users.noreply.github.com>
2024-08-28 14:05:47 +0000

    Output tsc build errors

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -27,7 +27,7 @@ const ctx = await context({
 			}
 
 			try {
-				execSync('tsc -p tsconfig.json');
+				execSync('tsc -p tsconfig.json', {stdio: 'inherit'});
 			} catch (e) {
 				console.error(e);
 			}
